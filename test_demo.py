ddc31589cb9127330bb2038b358e6d71b2bf3d17
tomtest1743 <850156124@qq.com>
2021-12-15 18:14:46 +0800

    update

--- test_demo.py
+++ test_demo.py
@@ -1,14 +1,23 @@
-#/root/python3 python3
+#/root/python3/bin python3
+#coding:utf-8
+import sys
+sys.path.append('/root/python3/lib/python3.7/site-packages')
+
 import pytest
 import allure
 
 @allure.feature("test_pytest")
-class TestDemo:
+class TestCase:
     @allure.story("test1")
     @pytest.fixture(scope='class', autouse=True)
     def test_one(self):
-        print('do test1')
+        assert 1 == 1
+        print("do test1")
 
     @allure.story("test2")
     def test_two(self):
-        print('do test2')
+        assert 2 == 2
+        print("do test2")
+
+if __name__ == '__main__':
+    pytest.main(['/root/python3/project/test_demo.py'])
